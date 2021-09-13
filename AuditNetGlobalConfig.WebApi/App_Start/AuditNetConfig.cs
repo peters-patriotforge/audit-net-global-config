0a6d1bbcb266679f8b4530cf6dd6333782aa9899
Peter Santos <peters@patriotforge.com>
2021-09-13 15:27:36 -0400

    Setup multiple dbcontexts based on answer for issue 435: https://github.com/thepirat000/Audit.NET/issues/435#issuecomment-897906584

--- AuditNetGlobalConfig.WebApi/App_Start/AuditNetConfig.cs
+++ AuditNetGlobalConfig.WebApi/App_Start/AuditNetConfig.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Audit.Core;
+using Audit.EntityFramework.Providers;
 using AuditNetGlobalConfig.Models;
+using AuditNetGlobalConfig.Security.Models;
 
 namespace AuditNetGlobalConfig.WebApi.App_Start
 {
@@ -12,20 +14,33 @@ namespace AuditNetGlobalConfig.WebApi.App_Start
         public static void Configure()
         {
             Audit.Core.Configuration.Setup()
-                .UseEntityFramework(_ => _
-                    .AuditTypeMapper(t => typeof(AuditLog))
-                    .AuditEntityAction<AuditLog>((ev, entry, entity) =>
+                .UseCustomProvider(new MultiEntityFrameworkDataProvider());
+
+            var dp = Audit.Core.Configuration.DataProvider as MultiEntityFrameworkDataProvider;
+
+            dp.AttachDataProvider<DatabaseContext>(new EntityFrameworkDataProvider(ef => ef
+                .UseDbContext<DatabaseContext>()
+                    .AuditTypeMapper(t => typeof(Models.AuditLog))
+                    .AuditEntityAction<Models.AuditLog>((ev, entry, entity) =>
                     {
                         entity.AuditData = entry.ToJson();
                         entity.EntityType = entry.EntityType.Name;
                         entity.AuditDate = DateTime.Now;
                         entity.TablePk = entry.PrimaryKey.First().Value.ToString();
                     })
-                    .IgnoreMatchedProperties(true));
-
-            Audit.EntityFramework.Configuration.Setup()
-                .ForContext<DatabaseContext>()
-                .UseOptOut();
+                    .IgnoreMatchedProperties(true)));
+            
+            dp.AttachDataProvider<SecurityDatabaseContext>(new EntityFrameworkDataProvider(ef => ef
+                .UseDbContext<SecurityDatabaseContext>()
+                    .AuditTypeMapper(t => typeof(Security.Models.AuditLog))
+                    .AuditEntityAction<Security.Models.AuditLog>((ev, entry, entity) =>
+                    {
+                        entity.AuditData = entry.ToJson();
+                        entity.EntityType = entry.EntityType.Name;
+                        entity.AuditDate = DateTime.Now;
+                        entity.TablePk = entry.PrimaryKey.First().Value.ToString();
+                    })
+                    .IgnoreMatchedProperties(true)));
         }
     }
 }
---
+++ AuditNetGlobalConfig.WebApi/App_Start/MultiEntityFrameworkDataProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Audit.Core;
+using Audit.EntityFramework;
+using Audit.EntityFramework.Providers;
+
+namespace AuditNetGlobalConfig.WebApi.App_Start
+{
+    public class MultiEntityFrameworkDataProvider : AuditDataProvider
+    {
+        // Store a list of Condition->DataProvider
+        private List<ValueTuple<Func<AuditEvent, bool>, EntityFrameworkDataProvider>> _providers = 
+            new List<ValueTuple<Func<AuditEvent, bool>, EntityFrameworkDataProvider>>();
+        // Adds a new data provider to the list, depending on the DbContext type T
+        public void AttachDataProvider<T>(EntityFrameworkDataProvider dataProvider)
+            where T : DbContext
+        {
+            Func<AuditEvent, bool> condition = ev => ev.GetEntityFrameworkEvent()?.GetDbContext() is T;
+            AttachDataProvider(condition, dataProvider);
+        }
+
+        public void AttachDataProvider(Func<AuditEvent, bool> condition, EntityFrameworkDataProvider dataProvider)
+        {
+            _providers.Add((condition, dataProvider));
+        }
+
+        public override object InsertEvent(AuditEvent auditEvent)
+        {
+            foreach ((Func<AuditEvent, bool> condition, EntityFrameworkDataProvider dataProvider) in _providers)
+            {
+                if (condition.Invoke(auditEvent))
+                {
+                    dataProvider.InsertEvent(auditEvent);
+                    break;
+                }
+            }
+            return null;
+        }
+
+        public async override Task<object> InsertEventAsync(AuditEvent auditEvent)
+        {
+            foreach ((Func<AuditEvent, bool> condition, EntityFrameworkDataProvider dataProvider) in _providers)
+            {
+                if (condition.Invoke(auditEvent))
+                {
+                    await dataProvider.InsertEventAsync(auditEvent);
+                    break;
+                }
+            }
+            return null;
+        }
+    }
+}
